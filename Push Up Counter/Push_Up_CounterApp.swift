0f9263ae2aed2cf6f13ab1a5981e211ab12b1092
fenuku kekeli <fenuku.kekeli8989@gmail.com>
2026-02-01 21:34:57 +0000

    Initial Commit

---
+++ Push Up Counter/Push_Up_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Push_Up_CounterApp.swift
+//  Push Up Counter
+//
+//  Created by Fenuku kekeli on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Push_Up_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
